121ff3d7e6b3e40913e87b9402837122ce56eecd
Cory House <housecor@gmail.com>
2022-12-05 13:58:44 -0600

    Implement change handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Input from "./reusable/Input";
 
 // Array of construction projects
@@ -14,24 +15,51 @@ const projects = [
   },
 ];
 
-type Project = {
+interface Project extends NewProject {
   id: number;
+}
+
+interface NewProject {
   name: string;
   description: string;
+}
+
+const newProject: NewProject = {
+  name: "",
+  description: "",
 };
 
 export default function App() {
+  const [project, setProject] = useState(newProject);
+
   // function renderProject(project: Project) {
   //   return <li key={project.id}>{project.name}</li>;
   // }
 
+  function onSubmit() {}
+
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setProject({ ...project, [event.target.id]: event.target.value });
+  }
+
   return (
     <>
       <h1>Projects</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <h2>Add Project</h2>
-        <Input label="Name" id="name" />
-        <Input label="Description" id="description" />
+        <Input
+          label="Name"
+          id="name"
+          value={project.name}
+          onChange={onChange}
+        />
+        <Input
+          label="Description"
+          id="description"
+          value={project.description}
+          onChange={onChange}
+        />
+        <button type="submit">Add Project</button>
       </form>
       <ul>
         {projects.map((project) => (
