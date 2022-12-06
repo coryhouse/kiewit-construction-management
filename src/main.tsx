8ff6a8b6489b45f417872eb0f7a63c5b0495058a
Cory House <housecor@gmail.com>
2022-12-06 15:41:15 -0600

    root error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import App from "./App";
 import "./index.css";
+import ErrorBoundary from "./reusable/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Toaster />
-    <App />
+    <ErrorBoundary>
+      <Toaster />
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
